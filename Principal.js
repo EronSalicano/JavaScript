00d39d4182b17bc63739e7b3bfd81f40987671f2
EronSalicano <161715505+EronSalicano@users.noreply.github.com>
2024-10-22 07:59:59 -0300

    Add files via upload

--- Principal.js
+++ Principal.js
@@ -1,4 +1,11 @@
-var titulo = document.querySelector(".titulo")
-titulo.textContent = ("aparecida nutricionista")
+var titulo = document.querySelector(".titulo");
+titulo.textContent = ("aparecida nutricionista");
 
-var paciente = document.querySelector("#primeiro-paciente")
+var paciente = document.querySelector("#primeiro-paciente");
+var tdpeso = paciente.querySelector("..info-pessoa");
+
+var peso = tdPeso.textContent;
+var altura = paciente.querySelector(".info-alura")
+
+var imc = peso / altura * altura;
+console.log(imc)
